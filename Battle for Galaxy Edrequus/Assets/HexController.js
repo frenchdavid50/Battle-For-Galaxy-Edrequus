d98193ad9cc6c2cc9943e50975b6dcfe2bafff28
sharebear42 <isaacschumann2@gmail.com>
2015-10-04 12:58:56 -0400

    Updating the HexController again

--- Battle for Galaxy Edrequus/Assets/HexController.js
+++ Battle for Galaxy Edrequus/Assets/HexController.js
@@ -11,10 +11,24 @@ public var rend: Renderer;
 function OnMouseEnter () {
 	rend.material.color = blue2;
 	transform.position.y = 0.2;
-	Debug.Log ("mouse enter" + Time.time + " s");
+	Debug.Log ("mouse enter " + Time.time + " s");
 }
 function OnMouseExit () {
 	rend.material.color = blue1;
 	transform.position.y = 0.0;
 }
 
+function OnTriggerEnter(movTrigger: Collider) {
+	if (movTrigger.gameObject.tag == "Player") {
+		rend.material.color = green1;
+		transform.position.y = 0.2;
+		
+		Debug.Log ("should be green " + Time.time + " s");
+	}	
+}
+function OnTriggerExit(movTrigger: Collider) {
+	if (movTrigger.gameObject.tag == "Player") {
+		rend.material.color = blue1;
+		transform.position.y = 0.0;
+	}
+}	
