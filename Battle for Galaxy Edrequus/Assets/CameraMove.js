02299851772c02edc66b653bcde0342db6a9c589
sharebear42 <isaacschumann2@gmail.com>
2015-10-04 12:07:00 -0400

    Adding CameraMove script

---
+++ Battle for Galaxy Edrequus/Assets/CameraMove.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+public var camSpeed: float = 1.5;
+
+function Update () {
+
+	var moveHorizontal: float = Input.GetAxis ("Horizontal") * camSpeed;
+	var moveVertical: float = Input.GetAxis ("Vertical") * camSpeed;
+	
+	moveHorizontal *= Time.deltaTime;
+	moveVertical *= Time.deltaTime;
+	
+	transform.Translate (moveHorizontal, moveVertical, 0);
+}
