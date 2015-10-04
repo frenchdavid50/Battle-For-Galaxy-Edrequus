70f28d230abf616a4867f796970b3ba580c2a84e
sharebear42 <isaacschumann2@gmail.com>
2015-10-04 12:04:10 -0400

    Adding HexController script

---
+++ Battle for Galaxy Edrequus/Assets/HexController.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var blue1: Color;
+public var blue2: Color;
+public var green1: Color;
+public var yellow1: Color;
+public var red1: Color;
+
+function OnMouseEnter () {
+	GetComponent.<Renderer>().material.color = blue2;
+	Debug.Log ("mouse enter" + Time.time + " s");
+}
+function OnMouseExit () {
+	GetComponent.<Renderer>().material.color = blue1;
+}
+
