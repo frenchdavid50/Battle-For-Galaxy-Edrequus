d06319eb2e7273793b01a9320390ef14769bbacf
sharebear42 <isaacschumann2@gmail.com>
2015-10-04 12:39:28 -0400

    HexController script update

--- Battle for Galaxy Edrequus/Assets/HexController.js
+++ Battle for Galaxy Edrequus/Assets/HexController.js
@@ -6,11 +6,15 @@ public var green1: Color;
 public var yellow1: Color;
 public var red1: Color;
 
+public var rend: Renderer;
+
 function OnMouseEnter () {
-	GetComponent.<Renderer>().material.color = blue2;
+	rend.material.color = blue2;
+	transform.position.y = 0.2;
 	Debug.Log ("mouse enter" + Time.time + " s");
 }
 function OnMouseExit () {
-	GetComponent.<Renderer>().material.color = blue1;
+	rend.material.color = blue1;
+	transform.position.y = 0.0;
 }
 
